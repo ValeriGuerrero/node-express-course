0efed2ecdb1ca59831f3a87a10395380332c475e
ValeriGuerrero <valeriguerrero22@outlook.com>
2025-04-05 17:38:05 -0500

    week 4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,54 +1,32 @@
 const express = require('express')
-const path = require('path')
-const { products } = require('./data')
-
 const app = express()
+const logger = require('./logger')
+const cookieParser = require('cookie-parser')
+
+const people = require('./public/routes/people')
+const auth = require('./public/routes/auth')
+
+app.use(logger)
 
 app.use(express.static('./public'))
 
-app.get('/api/v1/test', (req, res) => {
-    res.json({ message: "It worked!" });
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/api/people', people)
+app.use('/login', auth)
+
+app.get('/', (req, res) => {
+
+    res.send('Home')
 })
 
-app.get('/api/v1/products', (req, res) => {
-    const newProducts = products.map((p) => {
-        const { id, name, image, price, desc } = p
-        return { id, name, image, price, desc }
-    })
-    res.json(newProducts)
-})
+app.get('/about', (req, res) => {
 
-app.get('/api/v1/products/:productID', (req, res) => {
-    const idToFind = parseInt(req.params.productID);
-    const product = products.find((p) => p.id === Number(idToFind));
-    console.log("API route hit!");
-
-    if (!product) {
-        return res.status(404).json({ message: "Product was not found." })
-    }
-    res.json(product)
-})
-
-app.get('/api/v1/query', (req, res) => {
-    const { search, price, limit } = req.query
-    let sortedProducts = [...products]
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((p) => {
-            return p.name.includes(search)
-        })
-    }
-
-    if (price) {
-        sortedProducts = sortedProducts.filter((p) => {
-            return p.price < Number(price)
-        })
-    }
-
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    res.status(200).json(sortedProducts)
+    res.send('About')
 })
 
 app.all('*', (req, res) => {
@@ -58,3 +36,5 @@ app.all('*', (req, res) => {
 app.listen(3000, () => {
     console.log('server is listening on port 3000....')
 })
+
+module.exports = logger
